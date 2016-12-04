ca22ba43f9bd2fce7ae87e6e567b9a585735b431
Pascal Kleindienst <mail@pascalkleindienst.de>
2016-12-04 15:12:46 +0100

    added a facade loader

---
+++ src/Facades/Loader.php
@@ -0,0 +1,70 @@
+<?php
+namespace GeistPress\Helpers\Facades;
+
+use Mrubiosan\Facade\FacadeLoader;
+use Mrubiosan\Facade\ServiceLocatorAdapter\CallableAdapter;
+
+class Loader
+{
+    /**
+     * @var array
+     */
+    public $loaded = [];
+
+    /**
+     * @var array
+     */
+    private $facades = [];
+
+    /**
+     * Init FacadeLoader
+     */
+    public function __construct()
+    {
+        // Facades
+        $this->facades = [
+            'View' => function ($loader) {
+                if (!array_key_exists('View', $loader->loaded)) {
+                    $loader->loaded['View'] = new \GeistPress\Helpers\Services\View();
+                }
+
+                return $loader->loaded['View'];
+            }
+        ];
+
+        // Locator+Loader
+        $locator = new CallableAdapter(function($name) {
+            return $this->load($name);
+        });
+
+        FacadeLoader::init($locator);
+    }
+
+    /**
+     * Load a facade
+     *
+     * @param string $name
+     * @return mixed
+     */
+    private function load($name)
+    {   
+        // Facade does not exist
+        if (!array_key_exists($name, $this->facades)) {
+            throw new \InvalidArgumentException("No facade with name $name found");
+        }
+
+        // load facade
+        $class = $this->facades[$name];
+
+        if (is_callable($class)) {
+            return $class($this);
+        }
+
+        if (class_exists($class)) {
+            return new $class();
+        }
+
+        // neither callable nor class
+        throw new \InvalidArgumentException("Facade $name could not be instanciated");
+    }
+}
