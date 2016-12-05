807630f1319a45a24c8b24c916e47abf2aa6e0ac
Pascal Kleindienst <mail@pascalkleindienst.de>
2016-12-05 11:23:12 +0100

    added namespace support

--- src/Foundation/Loader.php
+++ src/Foundation/Loader.php
@@ -22,23 +22,25 @@ class Loader
      * Register all registerable classes of a directory
      *
      * @param string $path
+     * @param string|null $namespace Root namespace of $path
      * @throws \InvalidArgumentException if $path is not a valid directory
      */
-    public function register($path)
+    public function register($path, $namespace = null)
     {
         if (!file_exists($path) || !is_dir($path)) {
             throw new \InvalidArgumentException("$path is not a valid directory");
         }
         
         // load classes from $path
-        spl_autoload_register(function ($class) use ($path) {
-            include $path . DIRECTORY_SEPARATOR . $class . '.php';
+        spl_autoload_register(function ($class) use ($path, $namespace) {
+            include $path . DIRECTORY_SEPARATOR . str_replace($namespace, '', $class) . '.php';
         });
 
         // init all classes and call their register method
         foreach (glob($path . '/*.php') as $file) {
             $class = basename($file, '.php');
-
+            $class = is_null($namespace) ? $class : "$namespace\\$class";
+            
             if (class_exists($class)) {
                 $obj = new $class();
                     
